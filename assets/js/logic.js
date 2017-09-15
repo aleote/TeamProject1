907e8640ce63b4aeffae76dbb86d88d84c9e479c
annalisa leote <amleote@gmail.com>
2017-09-15 14:37:49 -0400

    added about and contact

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,3 +5,19 @@ var nxAppKey = "b27da09f2dfe35098a3ba4733fd361e8";
 
 // Google API key
 var gApiKey = "AIzaSyAjnWWbP30ssxxKP-jULse9lWmbR9AIaZ8";
+
+
+
+function expandAll(){
+  $(".collapsible-header").addClass("active");
+  $(".collapsible").collapsible({accordion: false});
+}
+
+function collapseAll(){
+  $(".collapsible-header").removeClass(function(){
+    return "active";
+  });
+  $(".collapsible").collapsible({accordion: true});
+  $(".collapsible").collapsible({accordion: false});
+}
+        
